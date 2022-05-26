918efce540014efc659814e44ae4c4d639c5cd39
archsinner <sinnervial@icloud.com>
2022-05-26 12:24:52 -0600

    idk

---
+++ Rust/temperature_conversion/target/rls/debug/deps/temperature_conversion-2f540f82d830d6ec.d
@@ -0,0 +1,5 @@
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\temperature_conversion\target\rls\debug\deps\temperature_conversion-2f540f82d830d6ec.rmeta: src\main.rs
+
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\temperature_conversion\target\rls\debug\deps\temperature_conversion-2f540f82d830d6ec.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/temperature_conversion/target/rls/debug/deps/temperature_conversion-62193cc10e18f488.d
@@ -0,0 +1,5 @@
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\temperature_conversion\target\rls\debug\deps\temperature_conversion-62193cc10e18f488.rmeta: src\main.rs
+
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\temperature_conversion\target\rls\debug\deps\temperature_conversion-62193cc10e18f488.d: src\main.rs
+
+src\main.rs:
