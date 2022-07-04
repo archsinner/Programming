5f819b533818f074f61fd599fb25ee87eadb3016
archsinner <sinnervial@icloud.com>
2022-07-04 15:02:31 -0400

    Program Updates

---
+++ Rust/target/rls/debug/deps/Rust-03a780576e370a74.d
@@ -0,0 +1,5 @@
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\target\rls\debug\deps\Rust-03a780576e370a74.rmeta: src\main.rs
+
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\target\rls\debug\deps\Rust-03a780576e370a74.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/target/rls/debug/deps/Rust-ec4f4029d2bc9f77.d
@@ -0,0 +1,5 @@
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\target\rls\debug\deps\Rust-ec4f4029d2bc9f77.rmeta: src\main.rs
+
+c:\Users\ficht\OneDrive\Documents\Programming\Rust\target\rls\debug\deps\Rust-ec4f4029d2bc9f77.d: src\main.rs
+
+src\main.rs:
