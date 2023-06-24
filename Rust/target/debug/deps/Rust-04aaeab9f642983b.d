56f1907fe4b51929215f0b26f0a7b8a9a3d84ed6
archsinner <74122523+archsinner@users.noreply.github.com>
2023-06-24 17:11:06 -0400

    update

---
+++ Rust/target/debug/deps/Rust-04aaeab9f642983b.d
@@ -0,0 +1,5 @@
+C:\Users\ficht\OneDrive\Documents\Programming\Rust\target\debug\deps\Rust-04aaeab9f642983b.rmeta: src\main.rs
+
+C:\Users\ficht\OneDrive\Documents\Programming\Rust\target\debug\deps\Rust-04aaeab9f642983b.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/target/debug/deps/Rust-5895613417862647.d
@@ -0,0 +1,5 @@
+C:\Users\ficht\OneDrive\Documents\Programming\Rust\target\debug\deps\Rust-5895613417862647.rmeta: src\main.rs
+
+C:\Users\ficht\OneDrive\Documents\Programming\Rust\target\debug\deps\Rust-5895613417862647.d: src\main.rs
+
+src\main.rs:
